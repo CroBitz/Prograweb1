230ded132c09032ed1cdf8f0066763cf54eade93
dabrianla <dabrianla@gmail.com>
2021-05-10 03:26:15 -0400

    creacion de tema oscuro, cambios de imagenes, arreglo de js
    me tome el tiempo de crear el modo oscuro para que la pagina contenga una estetica mas producida, los colores aun estan en desarrollo y planeo implementarlas en todas las paginas, por el momento solo esta en el main. quiero guardar y a lo que suba esto, sigo editando las demas paginas con la implementacion de los colores y el tema oscuro

--- js/Index.js
+++ js/Index.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    // set up hover panels
-    // although this can be done without JavaScript, we've attached these events
-    // because it causes the hover to be triggered when the element is tapped on a touch device
     $('.hover').hover(function(){
         $(this).addClass('flip');
     },function(){
         $(this).removeClass('flip');
     });
 });
+
+const btnswitch = document.querySelector('#switch');
+btnswitch.addEventListener('click' , () => {
+    document.body.classList.toggle('dark');
+    btnswitch.classList.toggle('active');
+})
