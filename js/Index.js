d08f41a738fe261ea45a8d93c6fd3a9bbf4ae5f9
dabrianla <dabrianla@gmail.com>
2021-05-11 03:57:44 -0400

    cambio de colores, y validaciones

--- js/Index.js
+++ js/Index.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
+
+    console.log("Prueba")
+    
     $('.hover').hover(function(){
         $(this).addClass('flip');
     },function(){
         $(this).removeClass('flip');
     });
-});
 
-const btnswitch = document.querySelector('#switch');
-btnswitch.addEventListener('click' , () => {
+    const btnswitch = document.querySelector('#switch');
+    btnswitch.addEventListener('click' , () => {
     document.body.classList.toggle('dark');
     btnswitch.classList.toggle('active');
-})
+    })
+
+
+});
+
